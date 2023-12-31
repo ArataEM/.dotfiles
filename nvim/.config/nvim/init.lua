d0d0a141b7be6a6059ba8690c8a6ac1d55d0566c
Artem Usenko <artem.usenko42@gmail.com>
2023-12-31 16:58:11 +0200

    configuration

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,9 +1,3 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=4")
-vim.cmd("set softtabstop=4")
-vim.cmd("set shiftwidth=4")
-vim.g.mapleader = " "
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -17,17 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {
-    { "catppuccin/nvim", name = "catppuccin", priority = 1001 },
-    { 'nvim-telescope/telescope.nvim', tag = '0.1.5', dependencies = { 'nvim-lua/plenary.nvim' } }
-}
-local opts = {}
+require("lazy").setup("plugins")
+require("remap")
+require("set")
 
-require("lazy").setup(plugins, opts)
-
-local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<C-p>", function() builtin.find_files({hidden=true}) end, {})
-vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-
-require("catppuccin").setup()
-vim.cmd.colorscheme "catppuccin"
---
+++ nvim/.config/nvim/lua/set.lua
@@ -0,0 +1,11 @@
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.hlsearch = false
+
+vim.g.mapleader = " " 
